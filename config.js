0f557cc24a0a0d30ed91a9369e69e30cb80c3288
melodieLondon <142746258+melodieLondon@users.noreply.github.com>
2026-03-17 23:08:22 +0000

    Update config.js
    update with actual gsheet

--- config.js
+++ config.js
@@ -5,5 +5,5 @@
 // Paste your Google Sheet published CSV URL here.
 // See README.md for how to get this URL.
 const CONFIG = {
-  SHEET_CSV_URL: "YOUR_GOOGLE_SHEET_CSV_URL_HERE"
+  SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ9zmDp86odxmRc3QrL06RS0Xcc2_xrYST5L0K4M6rh8gBFxMcvclKSw6AoLOMwn0Pn0xAp1ylRDsjI/pub?gid=0&single=true&output=csv"
 };
